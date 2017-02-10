2090c87c093ff3225ab23e23623ed2319bc91d3f
Paul Parton <paul.parton@agriwebb.com>
2017-02-10 12:33:40 +1100

    remove translate and add postinstall

--- src/aw-form.module.ts
+++ src/aw-form.module.ts
@@ -3,9 +3,6 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AwForm } from './aw-form';
 import { AwInputText, AwTextInputChangedEvent, AwTextInputControlDefinition } from './aw-input/aw-input';
-import {
-  TranslateModule, TranslateService, TranslateLoader, TranslateStaticLoader
-}from 'ng2-translate/ng2-translate';
 // import { AwDateTime, AwDateTimeChangedEvent, AwDateTimeControlDefinition } from './aw-date-time/aw-date-time';
 // import { AwReadOnlyText, AwReadOnlyTextControlDefinition } from './aw-readonly-text/aw-readonly-text';
 // import { AwSelect, AwSelectControlDefinition, AwSelectOption, AwSelectOptionChangedEvent } from './aw-select/aw-select';
@@ -20,15 +17,10 @@ function coreDirectives() {
 }
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    TranslateModule.forRoot(),
-  ],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   declarations: [coreDirectives()],
-  exports: [coreDirectives(), TranslateModule],
-  providers: [TranslateService]
+  exports: [coreDirectives()],
+  providers: []
 })
 export class AwFormModule {
   constructor(){
@@ -36,7 +28,7 @@ export class AwFormModule {
   public static forRoot(): ModuleWithProviders {
     return {
       ngModule: AwFormModule,
-      providers: [TranslateService],
+      providers: [],
     };
   }
 }
