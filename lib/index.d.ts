c5aa3b49f6c18103656bd4fcc4a4b9e0dd500626
Paul Parton <paul.parton@agriwebb.com>
2017-02-10 14:19:36 +1100

    add lib to repo

---
+++ lib/index.d.ts
@@ -0,0 +1,3 @@
+export { AwForm } from './aw-form';
+export { AwInputText, AwTextInputControlDefinition, AwTextInputChangedEvent } from './aw-input';
+export { AwFormModule } from './aw-form.module';
