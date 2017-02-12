33c56c58066eec70d4b426b7acedc4558b2667fe
Paul Parton <paul.parton@agriwebb.com>
2017-02-13 06:27:05 +1100

    update build

--- src/aw-input/aw-input.ts
+++ src/aw-input/aw-input.ts
@@ -27,8 +27,8 @@ export interface AwTextInputChangedEvent {
 
 @Component({
   selector: 'aw-input-text',
-  templateUrl: 'aw-input.html'
-  // template: require('./aw-input.html')
+  // templateUrl: 'aw-input.html'
+  template: require('./aw-input.html')
 })
 export class AwInputText {
 
